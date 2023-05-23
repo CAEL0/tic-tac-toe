a0362458057045c463ee09ebf1a94f96f13b0ba0
CAEL0 <kjongmin26@gmail.com>
2023-05-23 13:10:10 +0900

    feat: Add handler for '/'

--- server/server.go
+++ server/server.go
@@ -16,5 +16,10 @@ func New(port int) *Server {
 }
 
 func (s *Server) ListenAndServe() error {
+	http.HandleFunc("/", index)
 	return http.ListenAndServe(fmt.Sprintf(":%d", s.port), nil)
+}
+
+func index(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(w, "Hello, World!")
 }

669b8dd04021aed9ccb71dd949f7696f38175cf1
CAEL0 <kjongmin26@gmail.com>
2023-05-24 11:20:19 +0900

    feat: Load environment variable file path

--- server/cmd/main.go
+++ server/cmd/main.go
@@ -2,11 +2,17 @@ package main
 
 import (
 	"log"
+	"os"
 
 	"github.com/CAEL0/tic-tac-toe/server"
 )
 
 func main() {
+	envFile := "ENV_FILE"
+	envFilePath := os.Getenv(envFile)
+	if envFilePath == "" {
+		log.Fatalf("Environment variable (%s) is required.", envFile)
+	}
 	port := 8080
 	if serveError := server.New(port).ListenAndServe(); serveError != nil {
 		log.Fatalf("Failed toserver: %v", serveError)

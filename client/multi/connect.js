a5fb23d7d6ba9b69279010add6d19e71306e7065
CAEL0 <kjongmin26@gmail.com>
2023-05-29 22:25:44 +0900

    feat: Receive the board state from the server, validate it, and update it

--- client/multi/connect.js
+++ client/multi/connect.js
@@ -4,10 +4,24 @@ if (window["WebSocket"]) {
     conn = new WebSocket("ws://" + document.location.host + "/ws");
     conn.onclose = function (e) {};
     conn.onmessage = function (e) {
-        var receiveData = JSON.parse(e.data);
-        console.log(receiveData);
-    };
-    conn.onopen = function (e) {
-        conn.send(JSON.stringify(123));
+        var boardState = JSON.parse(e.data);
+
+        if (!Array.isArray(boardState)) {
+            console.log("The board is not an array:", boardState);
+            return;
+        }
+        if (boardState.length !== 9) {
+            console.log("The board is not of length 9:", boardState);
+            return;
+        }
+        if (
+            !boardState.every((n) => {
+                return n === 0 || n === 1 || n === 2;
+            })
+        ) {
+            console.log("The board contains inappropriate element:", boardState);
+            return;
+        }
+        window.updateBoard(boardState);
     };
 }
